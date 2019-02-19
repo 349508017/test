0563f2cac59645c9975d847fd8548af3c5390b27
T <18865477815@163.com>
2019-02-19 21:02:22 +0800

    test fethod

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by Tan
+ * Email 18865477815@163.com
+ * website https://www.xingchenw.cn
+ */
+require_once "vendor/autoload.php";
+\tan\test\Test::run('word');
---
+++ src/test/lib/Test.php
@@ -0,0 +1,8 @@
+<?php
+namespace tan\test;
+
+class Test{
+    public static function run(string $word){
+        echo "Hello ".$word;
+    }
+}
