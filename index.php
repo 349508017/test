3c54d1d9ae86feb8427a6701c9ddfaa3ddf0953b
会飞的毛毛虫 <18865477815@163.com>
2019-09-21 19:48:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Created by Tan
- * Email 18865477815@163.com
- * website https://www.xingchenw.cn
+ * Email tanchengjin9@163.com
+ * website https://www.tanchengjin.com
  */
 require_once "vendor/autoload.php";
 \tan\test\Test::run('word');
